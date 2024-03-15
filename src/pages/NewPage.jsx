682a7c5dae7926f608de9f22142073890c15c117
Piero Schenato <piero.schenato@gmail.com>
2024-03-15 17:44:55 +0100

    create part is done

--- src/pages/NewPage.jsx
+++ src/pages/NewPage.jsx
@@ -2,11 +2,25 @@
 import { useNavigate } from "react-router-dom";
 
 const NewPage = ({apartments,setApartments}) => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [maxGuests, setMaxGuests] = useState("");
+  const [location, setLocation] = useState("");
+  const [imageUrls, setImageUrls] = useState("");
+  const [rating, setRating] = useState("");
+  const [reviews, setReviews] = useState("");
+  const [hostName, setHostName] = useState("");
+  const [hostImageUrl, setHostImageUrl] = useState("");
+  const [favorite, setFavorite] = useState("");
 
+
+  const nav = useNavigate();
   
   // create the const to the inputs
 
-  const nav = useNavigate();
+ 
 
   //create the function to call the server and method POST to create the new apartment
 
@@ -60,6 +74,18 @@ const NewPage = ({apartments,setApartments}) => {
   return (
     <div  className="content-page mt-5 pt-5 pb-5">
       <form action="" className="mt-5 pt-5" onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Apartment Name" />
+        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" />
+        <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} placeholder="Price" />
+        <input type="number" value={maxGuests} onChange={(e) => setMaxGuests(e.target.value)} placeholder="Number of Guests" />
+        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Location" />
+        <input type="text" value={imageUrls} onChange={(e) => setImageUrls(e.target.value)} placeholder="Image" />
+        <input type="number" value={rating} onChange={(e) => setRating(e.target.value)} placeholder="Rating" />
+        <input type="text" value={reviews} onChange={(e) => setReviews(e.target.value)} placeholder="Reviews" />
+        <input type="text" value={hostName} onChange={(e) => setHostName(e.target.value)} placeholder="Host Name" />
+        <input type="text" value={hostImageUrl} onChange={(e) => setHostImageUrl(e.target.value)} placeholder="Host Image" />
+        <input type="text" value={favorite} onChange={(e) => setFavorite(e.target.value)} placeholder="Favorite" />
+        
 
         {/* //create all the inputs  */}
         <button type="submit">CREAR</button>
@@ -67,5 +93,6 @@ const NewPage = ({apartments,setApartments}) => {
     </div>
   )
 }
+
 
 export default NewPage
